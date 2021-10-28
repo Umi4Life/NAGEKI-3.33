943caa616865f9e5a4913f06bce612785df452e5
NanaNana <65470495+Nana0Nana@users.noreply.github.com>
2021-10-28 23:52:52 +0800

    Add files via upload
    ADD English

--- nageki2.0 code/nageki2.0.ino
+++ nageki2.0 code/nageki2.0.ino
@@ -10,7 +10,7 @@
 /*WS2812*/
 
 /*Pot*/
-int Sensor=A0;//将A0引脚取名为Sensor
+int Sensor=A0;//将A0引脚取名为Sensor/ A0 pin Named Sensor
 int SensorRead=0;
 int newdata=0;
 int inputValue=0;
@@ -34,6 +34,7 @@ const int SideR = 14;          // SKey right
 
 /* Encoder pin */
 /* 如果发现转动方向是反的就把3和2的位置调换一下*/
+/*Switch "3" and "2" if the spin way was wrong*/
 #define ENC_1_PIN_A 2
 #define ENC_1_PIN_B 3
 
@@ -44,7 +45,7 @@ Encoder encLeft(ENC_1_PIN_A, ENC_1_PIN_B);
 
 void setup() {
   Serial.begin(9600);
-  // 初始化按键引脚，如果没有上拉电阻，需要使用INPUT_PULLUP
+  // 初始化按键引脚，如果没有上拉电阻，需要使用INPUT_PULLUP/Pin Mode.
   pinMode(buttonPin1, INPUT_PULLUP);
   pinMode(buttonPin2, INPUT_PULLUP);
   pinMode(buttonPin3, INPUT_PULLUP);
@@ -55,51 +56,51 @@ void setup() {
   pinMode(FunctionR, INPUT_PULLUP);
   pinMode(SideL, INPUT_PULLUP);
   pinMode(SideR, INPUT_PULLUP);
-  // 初始化模拟键盘功能
+  // 初始化模拟键盘功能/Begin keyboard
   Keyboard.begin();
-  Keyboard.releaseAll(); //所有键盘按键松开
-  //初始化鼠标
+  Keyboard.releaseAll(); //所有键盘按键松开/Releasekeyboard
+  //初始化鼠标/Begin Mousemove
   Mouse.begin();
-  //归零编码器
+  //归零编码器/encoder write 0
   encLeft.write(0);
   }
 
 void Keys(){
-   if (digitalRead(buttonPin1) == LOW){ //按键1
+   if (digitalRead(buttonPin1) == LOW){ //按键1/Key1
     Keyboard.press('J');
    }
    else {
     Keyboard.release('J');
 }
 
-   if (digitalRead(buttonPin2) == LOW){ //按键2
+   if (digitalRead(buttonPin2) == LOW){ //按键2/Key2
     Keyboard.press('K');
    }
    else {
     Keyboard.release('K');
 }
 
-   if (digitalRead(buttonPin3) == LOW){ //按键3
+   if (digitalRead(buttonPin3) == LOW){ //按键3/Key3
     Keyboard.press('L');
    }
    else {
     Keyboard.release('L');
 }
-if (digitalRead(buttonPin4) == LOW){ //按键4
+if (digitalRead(buttonPin4) == LOW){ //按键4/Key 4
     Keyboard.press('A');
    }
    else {
     Keyboard.release('A');
 }
 
-   if (digitalRead(buttonPin5) == LOW){ //按键5
+   if (digitalRead(buttonPin5) == LOW){ //按键5/Key 5
     Keyboard.press('S');
    }
    else {
     Keyboard.release('S');
 }
 
-   if (digitalRead(buttonPin6) == LOW){ //按键6
+   if (digitalRead(buttonPin6) == LOW){ //按键6/Key 6
     Keyboard.press('D');
    }
    else {
@@ -107,13 +108,13 @@ if (digitalRead(buttonPin4) == LOW){ //按键4
 }
 }
 void Function(){
-  if (digitalRead(FunctionL) == LOW){ //功能键左
+  if (digitalRead(FunctionL) == LOW){ //功能键左/Menu L
     Keyboard.press('R');
    }
    else {
     Keyboard.release('R');
 }
-  if (digitalRead(FunctionR) == LOW){ //功能键右
+  if (digitalRead(FunctionR) == LOW){ //功能键右/Menu R
     Keyboard.press('U');
    }
    else {
@@ -121,13 +122,13 @@ void Function(){
 }
 }
 void Side(){
-    if (digitalRead(SideL) == LOW){ //侧键1
+    if (digitalRead(SideL) == LOW){ //侧键1/Side 1
     Keyboard.press('P');
    }
    else {
     Keyboard.release('P');
 }
-  if (digitalRead(SideR) == LOW){ //侧键2J
+  if (digitalRead(SideR) == LOW){ //侧键2/Side 2
     Keyboard.press('Q');
    }
    else {
@@ -150,7 +151,7 @@ void updateMousePositionLeft()
 
 void lever() 
 {
-    SensorRead=analogRead(Sensor);//读出Sensor的值并将它赋给SensorRead
+    SensorRead=analogRead(Sensor);//读出Sensor的值并将它赋给SensorRead/Read Sensor and wirte for SensorRead
     newdata=map(SensorRead,0,1023,0,255);
     if (newdata >= 255)
     {
